2ec08a0d462764a6d123690933e43072bf61f88f
CGatuna <charitygatuna@gmail.com>
2023-03-29 04:46:51 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int n = '0';
+int m = '0';
+for (n = 0; n <= 99; n++)
+{
+for (m = 0; m <= 99; m++)
+{
+if (n < m && n != m)
+{
+putchar((n / 10) + '0');
+putchar((n % 10) + '0');
+putchar(' ');
+putchar((m / 10) + '0');
+putchar((m % 10) + '0');
+if (n != 98 || m != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+return (0);
+}
