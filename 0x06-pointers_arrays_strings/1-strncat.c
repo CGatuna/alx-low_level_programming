c3f3754f66175c28e2c71cce1e746be0e45b5218
CGatuna <charitygatuna@gmail.com>
2023-03-30 08:53:16 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- *_strcat - concatenate two strings.
+ * _strcat - concatenate two strings.
  *@dest: destination string pointer.
- *@src: source string pointer
+ *@src: source string pointer.
+ *@n: number of bytes concatenated.
  * Return: pointer to destination string.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 int length_of_string, z;
 length_of_string = 0;
@@ -13,7 +14,7 @@ while (dest[length_of_string] != '\0')
 {
 length_of_string++;
 }
-for (z = 0; src[z] != '\0'; z++, length_of_string++)
+for (z = 0; z < n && src[z] != '\0'; z++, length_of_string++)
 {
 dest[length_of_string] = src[z];
 }
