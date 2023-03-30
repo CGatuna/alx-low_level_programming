6bef761fa51eb61897f00e639824f3d34f6adf2d
CGatuna <charitygatuna@gmail.com>
2023-03-30 11:29:26 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strncpy - copy string.
+ *@dest: destination string pointer.
+ *@src: source string pointer.
+ *@n: number of bytes concatenated.
+ * Return: number of bytes.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int count_of_bytes;
+for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
+dest[count_of_bytes] = src[count_of_bytes];
+dest[count_of_bytes] = '\0';
+return (dest);
+}
