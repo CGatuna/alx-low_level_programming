0dc0232fe30f4b9012327b00351e93a42b6a1f0a
CGatuna <charitygatuna@gmail.com>
2023-03-29 19:33:10 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - add two values, n and m.
+* @n: the value to be computed
+* @m: the value to be computed
+* Return: integer.
+*/
+int add(int n, int m)
+{
+return (n + m);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints all numbers from k to 98.
+* @k: value to start printig from.
+* Return: 0
+*/
+void print_to_98(int k)
+{
+if (k <= 98)
+{
+for (; k <= 98; k++)
+{
+if (k == 98)
+{
+printf("%d", k);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", k);
+}
+}
+}
+else
+{
+for (; k >= 98; k--)
+{
+if (k == 98)
+{
+printf("%d", k);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", k);
+}
+}
+}
+}
