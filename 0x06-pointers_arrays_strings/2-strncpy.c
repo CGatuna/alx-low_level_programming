fcb7ee94133f49ee9d47d6d63b6aa363d28736f4
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-03-30 22:07:35 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncpy - copy string.
+ *_strncpy - copy string.
  *@dest: destination string pointer.
  *@src: source string pointer.
  *@n: number of bytes used.
@@ -10,8 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int count_of_bytes;
 for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
+{
 dest[count_of_bytes] = src[count_of_bytes];
+{ 
 for (; count_of_bytes < n; count_of_bytes++)
 dest[count_of_bytes] = '\0';
 return (dest);
 }
+}
+}
+  
