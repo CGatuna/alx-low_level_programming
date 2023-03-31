ff94ea37e7658a11e9d9effaee000eba4a2bf40a
CGatuna <charitygatuna@gmail.com>
2023-03-30 19:29:11 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *string_toupper - changes all lowercase to uppercase.
+ *@x: pointer to string.
+ *Return: uppercase string.
+ */
+char *string_toupper(char *)
+{
+int string_legth;
+string_length = 0;
+while (x[string_length] != '\0')
+{
+if (x[string_length] >= 97 && x[string_length <= 122])
+{
+x[string_length] = x[string_length] - 32;
+}
+string_length++;
+}
+return (x);
+}
