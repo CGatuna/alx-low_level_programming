02d1432fef331e0cdf429cf0d6430361804707ab
CGatuna <charitygatuna@gmail.com>
2023-03-30 19:58:00 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  **string_toupper - changes all lowercase to uppercase.
-=======
  * *string_toupper - changes all lowercase to uppercase.
->>>>>>> f341a4289d31ffe6d9e91b33383a14c14579ccf7
  *@x: pointer to string.
  *Return: uppercase string.
  */
-char *string_toupper(char *)
+char *string_toupper(char *) 
 {
 int string_length;
 string_length = 0;
