8447f70216585d2ed4aae905053e5594d779f765
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-02 08:10:45 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *leet - encodes string to 1337.
+ **leet - encodes string to 1337.
  *Letters a and A are replaced by 4
  *Letters e and E are replaced by 3
  *Letters o and O are replaced by 0
  *Letters t and T are replaced by 7
  *Letters l and L are replaced by 1
- *@*s: pointer to string.
+ *@s: pointer to string.
  *Return: pointer to s.
  */
 char *leet(char *s)
