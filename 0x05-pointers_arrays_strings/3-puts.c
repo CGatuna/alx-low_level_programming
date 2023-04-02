836b0e215b33458a87733a5bda088a3cb958fc56
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-02 10:10:52 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
 **_puts - prints string.
-*_putchar - method defination
 *@str: string to be printed.
-*Return: string and new line.
+*Return: void.
 */
-void _putchar();
+void _putchar()
 void _puts(char *str)
 {
 int n = 0;
