766787471eaf7078acdd474681227b0376bd2ce0
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-02 06:35:09 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ s[string_count] == '{' || s[string_count] == '}')
 {
 if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
 {
-s[string_count + 1] = s[string_count + 1] - 32;
+s[string_count + 1] = s[string_count + 1] -32;
 }
 }
 string_count++;
