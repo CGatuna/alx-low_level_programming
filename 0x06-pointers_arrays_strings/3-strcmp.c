3c4bd76c2a4d5979604b2045960a37382a21901d
CGatuna <charitygatuna@gmail.com>
2023-04-01 20:01:49 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@ int _strcmp(char *s1, char *s2)
 {
 int k;
 k = 0;
-while (s1[k] != '\0'] && s2[k] != '\0')
+while (s1[k] != '\0' && s2[k] != '\0')
 {
 if (s1[k] != s2[k])
 {
 return (s1[k] - s2[k]);
 }
-k++
+k++;
 }
 return (0);
 }
