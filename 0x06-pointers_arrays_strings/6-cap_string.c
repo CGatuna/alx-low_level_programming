8d931da8764d418d01ffb227830b1adc10a8ed79
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-02 07:34:06 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,7 +24,7 @@ if (str[index - 1] == ' ' ||
     str[index - 1] == ')' ||
     str[index - 1] == '{' ||
     str[index - 1] == '}' ||
-    index == 0)
+    index = 0)
     str[index] -= 32;
     index++;
 }
