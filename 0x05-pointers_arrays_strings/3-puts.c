4c2f17e60ddade6849cbd17e2d5a393048a03da6
CGatuna <charitygatuna@gmail.com>
2023-04-01 23:31:58 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+**_puts - prints string.
+*@str: string to be printed.
+*Return: string and new line.
+*/
+void _puts(char *str)
+{
+int n = 0;
+while (str[n])
+{
+_putchar(str[n]);
+n++;
+}
+_putchar('\n');
+}
