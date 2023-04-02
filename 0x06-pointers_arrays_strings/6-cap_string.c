7afb69b9ff97af05afc241d937a40839a88dfb0f
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-02 07:21:05 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,32 @@
 #include "main.h"
 /**
-**cap_string - capitalizes words of a string.
-*@s: string to be capitalized.
+**cap_string - Capitalizes words of a string.
+*@str: string to be capitalized.
 *Return: capitalized string.
 */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-int string_count;
-string_count = 0;
-while (s[string_count] != '\0')
+index = 0;
+while (str[index])
 {
-if (s[0] >= 97 && s[0] <= 122)
-{
-s[0] = s[0] - 32;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+    str[index - 1] == '\t' ||
+    str[index - 1] == '\n' ||
+    str[index - 1] == ',' ||
+    str[index - 1] == ';' ||
+    str[index - 1] == '.' ||
+    str[index - 1] == '!' ||
+    str[index - 1] == '?' ||
+    str[index - 1] == '"' ||
+    str[index - 1] == '(' ||
+    str[index - 1] == ')' ||
+    str[index - 1] == '{' ||
+    str[index - 1] == '}' ||
+    index = 0)
+    str[index] -= 32;
+index++;
 }
-if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n' || s[string_count] == ','
-|| s[string_count] == ';' || s[string_count] == '.' || s[string_count] == '!' || s[string_count] == '?' || s[string_count] == '"'
-|| s[string_count] == '(' || s[string_count] == ')' 
-|| s[string_count] == '{' || s[string_count] == '}')
-{
-if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
-{
-s[string_count + 1] = s[string_count + 1] - 32;
-}
-}
-string_count++;
-}
-return (s);
+return (str);
 }
