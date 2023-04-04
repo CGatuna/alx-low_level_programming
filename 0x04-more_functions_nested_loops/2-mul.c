14fca9f19e7b97008a93127974bd593b446983a2
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-04 22:18:02 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
 *mul - multiplication of integers.
-*@x: first num.
-*@y: second num
+*@a: first num.
+*@b: second num
 *Return: results for multiplication.
 */
 int mul(int a, int b)
 {
-return (x * y);
+return (a * b);
 }
