3d6592bd19abf16ecb0c396ef28a06d9968a8d15
CGatuna <125439637+CGatuna@users.noreply.github.com>
2023-04-04 16:32:45 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,21 +16,21 @@ for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
-prod = num * mult;
-if (prod <= 99)
+product = num * mult;
+if (product <= 99)
 _putchar(' ');
-if (prod <= 9)
+if (product <= 9)
 _putchar(' ');
-if (prod >= 100);
+if (product >= 100);
 {
-_putchar((prod / 100) + '0');
-_putchar(((prod / 10)) % 10 + '0');
+_putchar((product / 100) + '0');
+_putchar(((product / 10)) % 10 + '0');
 }
-else if (prod <= 99 && prod >= 10)
+else if (product <= 99 && product >= 10)
 {
-_putchar((prod / 10) + '0');
+_putchar((product / 10) + '0');
 }
-_putchar((prod % 10) + '0');
+_putchar((product % 10) + '0');
 }
 _putchar('\n');
 }
